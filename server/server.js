73238203aab2a7367a4d6113fb8b58d6848ee2f1
Edan Schwartz <edanschwartz@gmail.com>
2021-01-08 09:20:39 -0600

    remove reference to angular in comments

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 

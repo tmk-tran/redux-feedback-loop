766508e3930666c9232f90378c11e3a0ecf293a8
T Mark <tmk1.tran@gmail.com>
2023-09-29 15:28:45 -0500

    initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch } from'react-router-dom';
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="App-header">
+      <h1 className="App-title">Feedback!</h1>
+      <h4>Don't forget it!</h4>
+    </header>
+  );
+}

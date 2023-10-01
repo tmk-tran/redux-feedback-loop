a2429d808c5efcd6585ee9e32e4c7cd648176559
T Mark <tmk1.tran@gmail.com>
2023-09-30 22:16:00 -0500

    added table, and styling to review component. centered table, added margins

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import React from "react";
+import { useSelector } from "react-redux";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import "./Review.css";
 
 export default function Review() {
   // bring in data from last component
@@ -18,44 +17,41 @@ export default function Review() {
   console.log("IN REVIEW COMPONENT, data received:", supportData);
   console.log("IN REVIEW COMPONENT, data received:", commentsData);
 
-  // state for this component
-//   const [review, setReview] = useState("");
-
-//   const dispatch = useDispatch();
-//   const history = useHistory();
-
-//   const handleSubmit = (event) => {
-//     console.log("clicked NEXT");
-
-//     event.preventDefault();
-
-//     dispatch({
-//       type: "SET_REVIEW",
-//       payload: {
-//         comments: comments,
-//       },
-//     });
-//     history.push("/review");
-
-//     // testing which feeling coming through
-//     console.log("Response for comments: ", comments);
-//   }; 
-
   return (
     <Card variant="outlined">
       <CardContent>
-        <Typography variant="h6">Review your feedback</Typography>
-        <form>
-          <TextField
-            label="Review"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-          <Button variant="contained" color="primary" fullWidth>
-            Submit
-          </Button>
-        </form>
+        <Typography variant="h6">Please review your feedback:</Typography>
+        <div className="review-info">
+          <table className="review-table">
+            <thead>
+              <tr>
+                <th>Section</th>
+                <th>Response</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Feeling</td>
+                <td>{feelingData.feeling}</td>
+              </tr>
+              <tr>
+                <td>Understanding</td>
+                <td>{understandingData.understanding}</td>
+              </tr>
+              <tr>
+                <td>Support</td>
+                <td>{supportData.support}</td>
+              </tr>
+              <tr>
+                <td>Comments</td>
+                <td>{commentsData.comments}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <Button variant="contained" color="primary" fullWidth>
+          Submit
+        </Button>
       </CardContent>
     </Card>
   );
